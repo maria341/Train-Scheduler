36c6804b5a002d2e5a49985a42a05c41da516b79
Maria may Roberts <mariamayr96@gmail.com>
2019-09-27 18:06:12 -0700

    index.html

---
+++ assets/javascript/train.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCrGLFTqnFbWSQ0JDsfNkfZ0yjPdTFc1SI",
+    authDomain: "train-scheduler-9fd69.firebaseapp.com",
+    databaseURL: "https://train-scheduler-9fd69.firebaseio.com",
+    projectId: "train-scheduler-9fd69",
+    storageBucket: "",
+    messagingSenderId: "1037539209968",
+    appId: "1:1037539209968:web:24ebcf8df98298d0948835"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
