e6692c2bdd4b9ced5aadad65bf12c57475e88fca
Maria may Roberts <mariamayr96@gmail.com>
2019-10-02 21:09:50 -0700

    index.html

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,4 +1,4 @@
-// Your web app's Firebase configuration
+// Web app's Firebase configuration
   var firebaseConfig = {
     apiKey: "AIzaSyCrGLFTqnFbWSQ0JDsfNkfZ0yjPdTFc1SI",
     authDomain: "train-scheduler-9fd69.firebaseapp.com",
@@ -24,12 +24,14 @@
  var trainTime = $("#firstTrain-input").val().trim();
  var freqTime = $("#frequency-input").val().trim();
 
-   var newTravelTrain = {
+  var newTravelTrain = {
      name: trainName,
      destination: trainDest,
      time: trainTime,
      frequency: freqTime
    };
+  
+  //Use the input collected from variables and push the response to firebase
    database.ref().push(newTravelTrain);
 
    console.log(newTravelTrain.name);
@@ -52,21 +54,23 @@
     var trainDest = childSnapshot.val().destination;
     var trainTime = childSnapshot.val().time;
     var freqTime = childSnapshot.val().frequency;
-
+   
+    //Time difference 
     var firstTimeConverted = moment(trainTime, "hh:mm a").subtract(1, "years");
     
     var currentTime = moment().format("HH:mm a");
     console.log("Current Time: " + currentTime);
 
     var trainTimeCurrentDiff = moment().diff(moment(firstTimeConverted), "minutes");
-    
+    //remainder
     var timeLeft = trainTimeCurrentDiff % freqTime;
-
+    //Times until next train arrive
     var minutesAway = freqTime - timeLeft;
-
+    //Next train arrival
     var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm a");
 
-   $("#train-table > tbody").append(
+   //Show data into the table when user input something
+    $("#train-table > tbody").append(
      "<tr><td>" + trainName + 
      "</td><td>" + trainDest + 
      "</td><td>" + freqTime +
